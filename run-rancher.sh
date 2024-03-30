d766568d345c9ead8f28db2269ffc96c38e77582
Aryan Blaauw <ablaauw@gmx.net>
2024-03-30 08:31:04 +0100

    Create run-rancher.sh

---
+++ run-rancher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo 'fetch k3d'
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+echo 'run rancher'
+docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:latest
+
+echo 'start cluster'
+k3d cluster create boxed -a 4
+
+echo 'done - now go to https://localhost'
+tail -f /dev/null
